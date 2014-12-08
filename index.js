13b4af478c63050f941e34d52aa3a73268cb3829
Travis McNeill <tavistock91@gmail.com>
2014-12-07 19:37:46 -0500

    modularize

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var scene;
 var camera;
 var renderer;
 
-var raycaster;
-var vector;
+var raycaster = new THREE.Raycaster();
+var vector = new THREE.Vector3();
 var brush; // UI element
 
 
-var onMouseDownPosition;
+var onMouseDownPosition = new THREE.Vector2();
 var radious = 600;
 var theta = 90;
 var onMouseDownTheta = 90;
@@ -31,22 +31,15 @@ var middle = new THREE.Vector3( 40.5 , 0, 40.5 );
 init();
 animate();
 
-function setUpScene(){
-  
-}
-
 function init() {
+  setUpScene();
 
-	scene = new THREE.Scene();
-	var WIDTH = window.innerWidth,
-    HEIGHT = window.innerHeight;
+  // Create a renderer and add it to the DOM.
+  renderer = new THREE.WebGLRenderer( { antialias:true } );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0x333F47, 1 );
 
-	// Create a renderer and add it to the DOM.
-  renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setSize(WIDTH, HEIGHT);
-  renderer.setClearColor(0x333F47, 1);
-
-  document.body.appendChild(renderer.domElement);
+  document.body.appendChild( renderer.domElement );
   document.body.style.margin = 0;
   document.body.style.overflow = "hidden";
 
@@ -58,12 +51,73 @@ function init() {
   info.innerHTML = '<span style="color: #444; background-color: #ddd; border-bottom: 1px solid #ddd; padding: 8px 10px; text-transform: uppercase;"><strong>click</strong>: add voxel, <strong>drag</strong>: rotate | <a href="javascript:save();">save</a></span>';
   document.body.appendChild( info );
 
-  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 1, 10000);
-  camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-  camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
-  camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-  camera.lookAt( middle );
-  scene.add(camera);
+
+  // Make the base cube
+  for ( var i = 0; i < divisor; i++ ) {
+    for ( var j = 0; j < divisor; j++ ) {
+      for ( var k = 0; k < divisor; k++ ) {
+        // if ( (j===1&&k===1) || (k===1&&i===1) || (j===1&&i===1)) { continue; }
+        var voxel = new THREE.Mesh(
+          new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize ),
+          new THREE.MeshLambertMaterial( { color: cubeColor, transparent: true, opacity:0.4 } )
+        );
+        voxel.position.set(
+          ( i * cubeSize ),
+          ( j * cubeSize ),
+          ( k * cubeSize )
+        );
+        scene.add( voxel );
+        base.push( voxel );
+        if ( i === 0 && j === 0 && k === 0 ) {
+          origin = voxel;
+        }
+      }
+    }
+  }
+  // make base fractal
+  fractal_cords = fractalize(base);
+  fractal_cords.map( function (arr) {
+    arr.map( function (cord) {
+      var voxel = new THREE.Mesh(
+        new THREE.BoxGeometry( miniCubeSize, miniCubeSize, miniCubeSize ),
+        new THREE.MeshLambertMaterial( { color: cubeColor } )
+      );
+      voxel.position.set( cord.x, cord.y, cord.z );
+      scene.add( voxel );
+      fractal[ cord.x + "," + cord.y + "," + cord.z ] = true;
+    });
+  });
+
+  brush = new THREE.Mesh(
+    new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize ),
+    new THREE.MeshLambertMaterial( { color: cubeColor, transparent: true, opacity: 0.4 } )
+  );
+  brush.position.y = 10000;
+  brush.overdraw = true;
+  scene.add( brush );
+
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild( stats.domElement );
+
+  document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
+
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+  document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+
+  document.addEventListener( 'keydown', onDocumentKeyDown, false );
+  document.addEventListener( 'keyup', onDocumentKeyUp, false );
+
+  window.addEventListener( 'resize', onResizeWindow, false );
+}
+
+function setUpScene(){
+  scene = new THREE.Scene();
+  
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 );
+  cameraAngle( camera, theta, phi, radious );
 
   // LIGHTS
   var ambientLight = new THREE.AmbientLight( 0x404040 );
@@ -82,78 +136,21 @@ function init() {
   directionalLight2.position.z = - 0.75;
   directionalLight2.position.normalize();
   scene.add( directionalLight2 );
+}
 
-  // Make the base cube
-  for (var i = 0; i < divisor; i++) {
-    for (var j = 0; j < divisor; j++) {
-      for (var k = 0; k < divisor; k++) {
-        // if ( (j===1&&k===1) || (k===1&&i===1) || (j===1&&i===1)) { continue; }
-        var voxel = new THREE.Mesh(
-          new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize ),
-          new THREE.MeshLambertMaterial( { color: cubeColor, transparent: true, opacity:0.4 } )
-        );
-        voxel.position.set(
-          ( i * cubeSize ),
-          ( j * cubeSize ),
-          ( k * cubeSize )
-        );
-        scene.add(voxel);
-        base.push(voxel);
-        if ( i === 0 && j === 0 && k === 0 ) {
-          origin = voxel;
-        }
-      }
-    }
-  }
-  // make base fractal
-  fractal_cords = fractalize(base);
-  fractal_cords.map(function (arr) {
-    arr.map( function (cord) {
-      var voxel = new THREE.Mesh(
-        new THREE.BoxGeometry( miniCubeSize, miniCubeSize, miniCubeSize ),
-        new THREE.MeshLambertMaterial({ color: cubeColor })
-      );
-      voxel.position.set(cord.x,cord.y,cord.z);
-      scene.add(voxel);
-      fractal[cord.x=","+cord.y+","+cord.z] = true;
-    });
-  });
+function cameraAngle (camera, theta, phi, radious) {
+    camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+    camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
+    camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
+    camera.updateMatrix();
+    camera.lookAt( middle );
+}
 
-  brush = new THREE.Mesh(
-    new THREE.BoxGeometry( cubeSize, cubeSize, cubeSize ),
-    new THREE.MeshLambertMaterial( { color: cubeColor, transparent: true, opacity: 0.4 } )
-  );
-  brush.position.y = 10000;
-  brush.overdraw = true;
-  scene.add( brush );
-
-  onMouseDownPosition = new THREE.Vector2();
-
-  raycaster = new THREE.Raycaster();
-  vector = new THREE.Vector3();
-
-  stats = new Stats();
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild( stats.domElement );
-
-  document.addEventListener( 'mousewheel', onDocumentMouseWheel, false );
-
-  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-  document.addEventListener( 'mousedown', onDocumentMouseDown, false );
-  document.addEventListener( 'mouseup', onDocumentMouseUp, false );
-
-  document.addEventListener( 'keydown', onDocumentKeyDown, false );
-  document.addEventListener( 'keyup', onDocumentKeyUp, false );
-
-  window.addEventListener('resize', function() {
-    var WIDTH = window.innerWidth,
-        HEIGHT = window.innerHeight;
-    renderer.setSize(WIDTH, HEIGHT);
-    camera.aspect = WIDTH / HEIGHT;
+function onResizeWindow () { 
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     animate();
-  });
 }
 
 
@@ -161,18 +158,12 @@ function onDocumentMouseMove (event) {
   event.preventDefault();
 
   if ( isMouseDown ) {
-
     theta = - ( ( event.clientX - onMouseDownPosition.x ) * 0.5 ) + onMouseDownTheta;
     phi = ( ( event.clientY - onMouseDownPosition.y ) * 0.5 ) + onMouseDownPhi;
 
     phi = Math.min( 180, Math.max( 0, phi ) );
 
-    camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-    camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
-    camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-    camera.updateMatrix();
-    camera.lookAt( middle );
-
+    cameraAngle( camera, theta, phi, radious );
   }
 
   vector.set(
@@ -222,18 +213,18 @@ function onDocumentMouseUp (event) {
         scene.add( voxel );
         base.push( voxel );
 
-        fractal_cords = fractalize(base);
-        fractal_cords.map(function (arr) {
+        fractal_cords = fractalize( base );
+        fractal_cords.map( function (arr) {
           arr.map( function (cord) {
-            cord_name = cord.x+","+cord.y+","+cord.z;
-            if (fractal[cord_name] !== true) {
+            cord_name = cord.x + "," + cord.y + "," + cord.z;
+            if ( fractal[ cord_name ] !== true ) {
               var voxel = new THREE.Mesh(
               new THREE.BoxGeometry( miniCubeSize, miniCubeSize, miniCubeSize ),
-              new THREE.MeshLambertMaterial({ color: cubeColor })
+              new THREE.MeshLambertMaterial( { color: cubeColor } )
               );
-              voxel.position.set(cord.x,cord.y,cord.z);
-              scene.add(voxel);
-              fractal[cord_name] = true;
+              voxel.position.set( cord.x, cord.y, cord.z );
+              scene.add( voxel );
+              fractal[ cord_name ] = true;
             }
           });
         });
@@ -249,15 +240,10 @@ function onDocumentMouseWheel( event ) {
 
   radious -= event.wheelDeltaY;
 
-  camera.position.x = radious * Math.sin( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-  camera.position.y = radious * Math.sin( phi * Math.PI / 360 );
-  camera.position.z = radious * Math.cos( theta * Math.PI / 360 ) * Math.cos( phi * Math.PI / 360 );
-  camera.updateMatrix();
-  camera.lookAt( middle );
+  cameraAngle( camera, theta, phi, radious );
 
   interact();
   animate();
-
 }
 
 function onDocumentKeyDown( event ) {
@@ -289,29 +275,41 @@ function interact () {
 
 function animate() {
   // requestAnimationFrame( animate );
-  renderer.render(scene, camera);
+  renderer.render( scene, camera );
   stats.update();
  }
 
 function fractalize (base_vectors) {
   var size = origin.geometry.parameters.width;
   var origin_vector = origin.position;
-  offset_base = createOffsetMap(base_vectors, origin_vector, size);
-  fractal_base = scaleOffsetMap(base_vectors, offset_base, size);
+  offset_base = createOffsetMap( base_vectors, origin_vector, size );
+  fractal_base = scaleOffsetMap( base_vectors, offset_base, size );
   return fractal_base;
 }
 
  function createOffsetMap (base_vectors, origin_vector, origin_size) {
   return base_vectors.map( function (vector) {
     var offset = new THREE.Vector3();
-    offset.subVectors(origin_vector, vector.position);
-    offset.divideScalar(origin_size);
+    offset.subVectors( origin_vector, vector.position );
+    offset.divideScalar( origin_size );
     return offset;
   });
  }
 
+ function scaleOffsetMap (base_vectors, offset_base, scale) {
+  return base_vectors.map( function (vector) {
+    return offset_base.map( function (offset) {
+      var scaled_clone = offset.clone();
+      scaled_clone.multiplyScalar( -scale/divisor ).add( vector.position );
+      scaled_clone.addScalar( -scale/magicNumber( divisor ) );
+      // that 3 at the end is magic and only works for a divisor of 3 for a divisor of 2:4, 3:3, 4:2.66
+      return scaled_clone;
+    });
+  });
+ }
+
 function magicNumber (num) {
-  switch (num) {
+  switch ( num ) {
     case 2:
       return 4;
     case 3:
@@ -321,20 +319,8 @@ function magicNumber (num) {
   }
 }
 
- function scaleOffsetMap (base_vectors, offset_base, scale) {
-  return base_vectors.map( function (vector) {
-    return offset_base.map(function (offset) {
-      var scaled_clone = offset.clone();
-      scaled_clone.multiplyScalar(-scale/divisor).add(vector.position);
-      scaled_clone.addScalar(-scale/magicNumber(divisor));
-      // that 3 at the end is magic and only works for a divisor of 3 for a divisor of 2:4, 3:3, 4:2.66
-      return scaled_clone;
-    });
-  });
- }
-
 function save () {
   brush.position.y = 10000;
   animate();
-  window.open( renderer.domElement.toDataURL('image/png'), 'mywindow' );
+  window.open( renderer.domElement.toDataURL( 'image/png' ), 'mywindow' );
 }
